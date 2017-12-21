55d5353f446bb3d544e3667be97638af9d38dd21
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-12-21 11:40:34 +0000

    ログイン機能

--- db/migrate/20171220070107_devise_create_users.rb
+++ db/migrate/20171220070107_devise_create_users.rb
@@ -2,9 +2,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: "test@test.com"
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :name,               null: false, default: ""
+      t.boolean :admin,             default: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -35,9 +35,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    #add_index :users, :email,                unique: true
+    add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :name, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
