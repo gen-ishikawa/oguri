c02efb8198bc40e5157e86c0aa64e0f9d96ff4ca
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-12-25 07:24:16 +0000

    バリデーション ファイルサイズ

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,4 +1,6 @@
 class Notice < ActiveRecord::Base
     validates :title, presence: true
+    # 投稿の際の画像のサイズをバリデーション
+    validates :image, file_size: {maximum: 1.megabytes.to_i} # 最大1MBに制限
     mount_uploader :image, ImagesUploader
 end
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Oguri
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.time_zone = 'Tokyo'
+
+    # carrierwaveのファイルサイズのバリデーション
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
